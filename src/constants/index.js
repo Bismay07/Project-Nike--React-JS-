a4fe313e17e71d2fb61822abeef5f7468dc15f6b
Bismay Bibhu Prakash <bismayssp1540@gmail.com>
2024-01-30 18:28:58 +0530

    Name Change

--- src/constants/index.js
+++ src/constants/index.js
@@ -79,7 +79,7 @@ export const reviews = [
     },
     {
         imgURL: customer2,
-        customerName: 'Sweeta Mishra',
+        customerName: 'Earl Doe',
         rating: 4.5,
         feedback: "The product not only met but exceeded my expectations. I'll definitely be a returning customer!"
     }
